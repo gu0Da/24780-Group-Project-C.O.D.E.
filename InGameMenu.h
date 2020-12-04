cb275f6e399204721cd31a2b2c13ceb51b41ec32
luxu15 <71571268+luxu15@users.noreply.github.com>
2020-12-03 19:46:36 -0500

    Add files via upload

---
+++ InGameMenu.h
@@ -0,0 +1,43 @@
+#ifndef INGAMEMENU_H_IS_INCLUDED
+#define INGAMEMENU_H_IS_INCLUDED
+
+class TextString
+{
+protected:
+	char* str;
+public:
+	TextString();
+	TextString(const TextString& incoming);
+	~TextString();
+	void CleanUp(void);
+
+	TextString& operator=(const TextString& incoming);
+
+	void CopyFrom(const TextString& incoming);
+
+	void Add(char c);
+	void Add(const char str[]);
+	void BackSpace(void);
+
+	void Set(const char incoming[]);
+	const char* GetPointer(void) const;
+
+	int Strlen(void) const;
+
+	const char* Fgets(FILE* fp);
+};
+
+class InGameMenu
+{
+public:
+	int level;
+	int key;
+	TextString showlevel;
+	TextString showweapon;
+	TextString showscore;
+	TextString showresult;
+	void AddText(int level, int score, int health, char* weapon);
+	void Display(int level, int score, int health, char* weapon);
+};
+#pragma once
+#endif
